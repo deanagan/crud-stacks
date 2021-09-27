4583b14b3cc120caa6ccc4066f6ebed0142ed6d2
Dean Agan <agandfr@gmail.com>
2021-09-27 17:14:58 +1000

    Fixed docker config

--- react-frontend/src/Appsettings.ts
+++ react-frontend/src/Appsettings.ts
@@ -3,5 +3,5 @@ export const server =
   process.env.REACT_APP_ENV === 'production'
     ? 'https://todo-backend-app.azurewebsites.net'
     : process.env.REACT_APP_ENV === 'dockerprod'
-    ? 'https://localhost:8090'
+    ? 'http://localhost:8090'
     : 'https://localhost:5001';
