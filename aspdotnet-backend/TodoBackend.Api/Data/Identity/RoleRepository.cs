7f5373ab933db9d23e65d1fba91c0bcba3f199d8
Dean Agan <agandfr@gmail.com>
2021-10-13 19:25:09 +1100

    Implement interface

--- aspdotnet-backend/TodoBackend.Api/Data/Identity/RoleRepository.cs
+++ aspdotnet-backend/TodoBackend.Api/Data/Identity/RoleRepository.cs
@@ -60,128 +60,48 @@ namespace TodoBackend.Api.Data.Identity
             }
         }
 
-        private Role AddRole(Role role)
-        {
-            var sql = @"
-                        declare @Outcome table (
-                            Id int,
-                            UniqueId uniqueidentifier,
-                            Created datetime,
-                            Updated datetime
-                        );
-                        insert into dbo.Roles (UniqueId, Name, NormalizedName, Description)
-                        output inserted.Id, inserted.UniqueId, inserted.Created, inserted.Updated into @Outcome
-                        values (NEWID(), @Name, @NormalizedName, @Description);
-
-                        select @Id = Id,
-                               @UniqueId = UniqueId,
-                               @Created = Created,
-                               @Updated = Updated
-                        from @Outcome;
-                        ";
-
-
-            using (var conn = new SqlConnection(_connectionString))
-            {
-                var parameter = new DynamicParameters();
-                parameter.Add("@Id", null, DbType.Int32, ParameterDirection.Output);
-                parameter.Add("@UniqueId", null, DbType.Guid, ParameterDirection.Output);
-                parameter.Add("@Created", null, DbType.DateTime, ParameterDirection.Output);
-                parameter.Add("@Updated", null, DbType.DateTime, ParameterDirection.Output);
-                parameter.Add("@Name", role.Name);
-                parameter.Add("@NormalizedName", role.NormalizedName);
-                parameter.Add("@Description", role.Description);
-
-                conn.Execute(sql, parameter);
-
-                var newRole = new Role()
-                {
-                    Id = parameter.Get<int>("@Id"),
-                    UniqueId = parameter.Get<Guid>("@UniqueId"),
-                    Name = role.Name,
-                    Description = role.Description,
-                    Created = parameter.Get<DateTime>("@Created"),
-                    Updated = parameter.Get<DateTime>("@Updated")
-                };
-
-                return newRole;
-            }
-        }
-
-        private Role UpdateRole(Guid guid, Role role)
-        {
-            var sql = @"
-                        declare @Outcome table (
-                            Id int,
-                            Name nvarchar(256),
-                            Description nvarchar(max),
-                            Created datetime,
-                            Updated datetime
-                        );
-
-                        update r
-                        set r.Name = coalesce(@Name, r.Name),
-                            r.NormalizedName = UPPER(coalesce(@Name, r.Name)),
-                            r.Description = coalesce(@Description, r.Description),
-                            r.Updated = getutcdate()
-                        output inserted.Id,
-                               inserted.Name,
-                               inserted.Description,
-                               inserted.Created,
-                               inserted.Updated
-                        into @Outcome
-                        from dbo.Roles r
-                        where r.UniqueId = @UniqueId
-
-                        select @Id = Id,
-                               @Created = Created,
-                               @Updated = Updated,
-                               @NewName = Name,
-                               @NewDescription = Description
-                        from @Outcome;
-                        ";
-
-
-            using (var conn = new SqlConnection(_connectionString))
-            {
-                var parameter = new DynamicParameters();
-                parameter.Add("@Id", null, DbType.Int32, ParameterDirection.Output);
-                parameter.Add("@Created", null, DbType.DateTime, ParameterDirection.Output);
-                parameter.Add("@Updated", null, DbType.DateTime, ParameterDirection.Output);
-                parameter.Add("@NewName", null, DbType.String, ParameterDirection.Output, 256);
-                parameter.Add("@NewDescription", null, DbType.String, ParameterDirection.Output, -1);
-
-                parameter.Add("@UniqueId", guid);
-                parameter.Add("@Name", role.Name);
-                parameter.Add("@Description", role.Description);
-
-                conn.Execute(sql, parameter);
-
-                var updatedRole = new Role()
-                {
-                    Id = parameter.Get<int>("@Id"),
-                    UniqueId = guid,
-                    Name = parameter.Get<string>("@NewName"),
-                    Description = parameter.Get<string>("@NewDescription"),
-                    Created = parameter.Get<DateTime>("@Created"),
-                    Updated = parameter.Get<DateTime>("@Updated")
-                };
-
-                return updatedRole;
-            }
-        }
-
         public Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            AddRole(role);
+
+            var sql = @"insert into dbo.Roles (UniqueId, Name, NormalizedName, Description)
+                        values (NEWID(), @Name, @NormalizedName, @Description);
+                        ";
+
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", role.Name);
+                parameter.Add("@NormalizedName", role.NormalizedName);
+                parameter.Add("@Description", role.Description);
+                conn.Execute(sql, parameter);
+            }
+
             return Task.FromResult(IdentityResult.Success);
         }
 
         public Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            UpdateRole(role.UniqueId, role);
+            var sql = @"update r
+                        set r.Name = coalesce(@Name, r.Name),
+                            r.NormalizedName = UPPER(coalesce(@Name, r.Name)),
+                            r.Description = coalesce(@Description, r.Description),
+                            r.Updated = getutcdate()
+                        from dbo.Roles r
+                        where r.UniqueId = @UniqueId
+                        ";
+
+
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                var parameter = new DynamicParameters();
+                parameter.Add("@UniqueId", role.UniqueId);
+                parameter.Add("@Name", role.Name);
+                parameter.Add("@Description", role.Description);
+
+                conn.Execute(sql, parameter);
+            }
             return Task.FromResult(IdentityResult.Success);
         }
 
