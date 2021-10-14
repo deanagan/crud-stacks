3c86830ed0c36bd37433712452d58c92fb90c289
Dean Agan <agandfr@gmail.com>
2021-10-14 21:35:52 +1100

    Update sql and schema to include UserRole

--- aspdotnet-backend/TodoBackend.Api.Database/SqlScripts/01-Create.sql
+++ aspdotnet-backend/TodoBackend.Api.Database/SqlScripts/01-Create.sql
@@ -35,6 +35,17 @@ CREATE TABLE [dbo].[Users](
  )
 GO
 
+
+CREATE TABLE [dbo].[UserRole]
+(
+	[UserId] INT NOT NULL,
+	[RoleId] INT NOT NULL
+  PRIMARY KEY ([UserId], [RoleId]),
+  CONSTRAINT [FK_UserRole_User] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
+  CONSTRAINT [FK_UserRole_Role] FOREIGN KEY ([RoleId]) REFERENCES [Roles]([Id])
+)
+GO
+
 CREATE INDEX [IX_Users_NormalizedUserName] ON [dbo].[Users] ([NormalizedUserName])
 GO
 
