b0644716a5ffb76f04d4dd8c4a37a463fb292085
Dean Agan <agandfr@gmail.com>
2021-10-21 19:55:09 +1100

    Adding some action types

---
+++ react-frontend/src/store/action-types/rolesActionTypes.ts
@@ -0,0 +1,7 @@
+export enum RolesActionTypes {
+    GET_ROLE = "GET_ROLE",
+    UPDATE_ROLE = "UPDATE_ROLE",
+    ADD_ROLE = "ADD_ROLE",
+    DELETE_ROLE = "DELETE_ROLE",
+    GET_ALL_ROLES = "GET_ALL_ROLES"
+}
---
+++ react-frontend/src/store/action-types/usersActionTypes.ts
@@ -0,0 +1,7 @@
+export enum UsersActionTypes {
+    GET_USER = "GET_USER",
+    UPDATE_USER = "UPDATE_USER",
+    ADD_USER = "ADD_USER",
+    DELETE_USER = "DELETE_USER",
+    GET_ALL_USERS = "GET_ALL_USERS"
+}
