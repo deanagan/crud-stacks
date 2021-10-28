2f67b71fdc9a12734f6fcbfe0412b149170bef05
Dean Agan <agandfr@gmail.com>
2021-10-28 20:17:53 +1100

    Add reducer annotation. Format document properly

--- react-frontend/src/store/reducers/todoReducer.ts
+++ react-frontend/src/store/reducers/todoReducer.ts
@@ -1,49 +1,60 @@
 import { TodoActionTypes } from "../action-types/todoActionTypes";
 import { TodoAction } from "../actions/todoActions";
 import { TodoState } from "../../types";
+import { Reducer } from "redux";
 
 const initialState: TodoState = {
-    todos: [],
-    errors: undefined,
-    loading: false
-  }
+  todos: [],
+  errors: undefined,
+  loading: false,
+};
 
-const reducer = (state: TodoState = initialState, action: TodoAction) => {
-    switch (action.type) {
-        case TodoActionTypes.ADD_TODO_ENTRY:
-            return {
-                ...state,
-                todos: [...state.todos, action.todo]
-            }
-        case TodoActionTypes.UPDATE_TODO_STATE:
-            return {
-                ...state,
-                todos: state.todos.map(e => e.uniqueId === action.uniqueId ?
-                    { ...e, isDone: action.isDone } : e)
-            }
-        case TodoActionTypes.GET_TODO_ENTRIES:
-            return {
-                ...state,
-                todos: action.todos
-            }
-        case TodoActionTypes.DELETE_TODO_ENTRY:
-            return {
-                ...state,
-                todos: state.todos.filter(e => e.uniqueId !== action.uniqueId)
-            }
-        case TodoActionTypes.UPDATE_TODO_ASSIGNEE:
-            return {
-                ...state,
-                todos: state.todos.map(e => e.uniqueId === action.uniqueId ?
-                    { ...e, assignee: {
-                        uniqueId: action.uniqueId,
+const reducer: Reducer<TodoState, TodoAction>  = (state: TodoState = initialState, action: TodoAction) => {
+  switch (action.type) {
+    case TodoActionTypes.ADD_TODO_ENTRY:
+      return {
+        ...state,
+        todos: [...state.todos, action.todo],
+      };
+    case TodoActionTypes.UPDATE_TODO_STATE:
+      return {
+        ...state,
+        todos: state.todos.map((e) =>
+          e.uniqueId === action.uniqueId ? { ...e, isDone: action.isDone } : e
+        ),
+      };
+    case TodoActionTypes.GET_TODO_ENTRIES:
+      return {
+        ...state,
+        todos: action.todos,
+      };
+    case TodoActionTypes.DELETE_TODO_ENTRY:
+      return {
+        ...state,
+        todos: state.todos.filter((e) => e.uniqueId !== action.uniqueId),
+      };
+    case TodoActionTypes.UPDATE_TODO_ASSIGNEE:
+      return {
+        ...state,
+        todos: state.todos.map((e) =>
+          e.uniqueId === action.uniqueId
+            ? {
+                ...e,
+                assignee:
+                  action.assigneeGuid !== null
+                    ? {
+                        uniqueId: action.assigneeGuid,
                         firstName: action.firstName,
-                        lastName: action.lastName
-                    } } : e)
-            }
-        default:
-            return state;
-    }
-}
+                        lastName: action.lastName,
+                      }
+                    : null,
+              }
+            : e
+        )
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
