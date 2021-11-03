d1d5139f3490dafab16961ce7be74e82c84730f1
Dean Agan <agandfr@gmail.com>
2021-11-03 19:26:10 +1100

    Adding a basic not found page

---
+++ react-frontend/src/pages/notfound.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Header = styled.h1`
+  justify-content: center;
+  padding-top: 55px;
+`;
+
+export const NotFound = () => {
+  return <Header>Page not found!</Header>;
+};
--- react-frontend/src/routes/routes.tsx
+++ react-frontend/src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Login, Contact, Home } from "../pages";
+import { Login, Contact, Home, NotFound } from "../pages";
 import { RoutePaths } from "./paths";
 
 export const RoutesWrapper = () => (
@@ -7,5 +7,6 @@ export const RoutesWrapper = () => (
     <Route path={RoutePaths.home} element={<Home />} />
     <Route path={RoutePaths.contact} element={<Contact />} />
     <Route path={RoutePaths.login} element={<Login />} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 );
