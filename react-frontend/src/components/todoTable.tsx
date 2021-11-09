030c5fcb8fd6bc4351486da39ba311d23d613070
Dean Agan <agandfr@gmail.com>
2021-11-09 20:25:22 +1100

    Add assignee drop down

--- react-frontend/src/components/todoTable.tsx
+++ react-frontend/src/components/todoTable.tsx
@@ -1,26 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { memo, FC, useState } from "react";
+import { memo, FC, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { DeleterAction } from ".";
 import { Table, ToggleSwitch } from "../design-system/molecules";
 import { State, todoActionCreators } from "../store";
 import { uuidv4Type } from "../types";
+import { AssigneeDropDown } from "./assigneeDropDown";
 
 
-interface TodoTableProp {
-    switchUniqueId: uuidv4Type;
-    initialState: boolean;
-    updateSwitchStage: (id: uuidv4Type, state: boolean) => void;
-  }
-
-
-export const TodoTable: FC<TodoTableProp> = memo(() => {
+export const TodoTable: FC = memo(() => {
   //export const TodoTable: FC = (() => { // using this line will cause the table to re-render
   const dispatch = useDispatch();
   const { updateTodoState } = bindActionCreators(todoActionCreators, dispatch);
   const { todos } = useSelector((state: State) => state.todo);
   const nTodos = todos.length;
+
+  useEffect(() => {
+    dispatch(todoActionCreators.getTodos());
+  }, [dispatch]);
+
+
 
   return nTodos > 0 ? (
     <Table
@@ -37,9 +37,10 @@ export const TodoTable: FC<TodoTableProp> = memo(() => {
             />
         ),
         deleter: (<DeleterAction uniqueId = {todo.uniqueId as uuidv4Type}/>),
+        assignee: (<AssigneeDropDown uniqueId={todo.uniqueId as uuidv4Type} />)
       }))}
-      columnLabels={["Summary", "Detail", "Completed", "Status", "Delete"]}
-      rowFields={["summary", "detail", "isDone", "switch", "deleter"]}
+      columnLabels={["Summary", "Detail", "Completed", "Status", "Delete", "Assignee"]}
+      rowFields={["summary", "detail", "isDone", "switch", "deleter", "assignee"]}
     />
   ) : (
     <h1>No todos!</h1>
