ee34246eb9230bc1952d12bc3b465dd9980b3572
Dean Agan <agandfr@gmail.com>
2021-11-18 17:50:13 +1100

    Update generation script

--- aspdotnet-backend/TodoBackend.Api.Database/SqlScripts/TodoGenerator.py
+++ aspdotnet-backend/TodoBackend.Api.Database/SqlScripts/TodoGenerator.py
@@ -2,8 +2,6 @@ import uuid
 import random
 import string
 
-cmd_fmt = 'INSERT INTO [dbo].[Todo]([Id], [UniqueId], [Summary], [Detail], [IsDone], [AssigneeGuid])'
-
 summary_gen = lambda: ''.join(random.choices(string.ascii_letters + string.digits, k=8))
 detail_gen = lambda: ''.join(random.choices(string.ascii_letters + string.digits + ' ', k=50))
 is_done_gen = lambda: random.choice(range(2))
@@ -12,9 +10,8 @@ is_done_gen = lambda: random.choice(range(2))
 def get_random_entries(index):
     return (index, uuid.uuid4(), summary_gen(), detail_gen(), is_done_gen())
 
-
-print(cmd_fmt)
-for i in range(1, 1001):
+total_todos = 20
+for i in range(1, total_todos):
     id, unique_id, summary, detail, is_done = get_random_entries(i)
-    value_fmt = f"({id}, '{unique_id}', '{summary}', '{detail}', {is_done}, null),"
+    value_fmt = f"({id}, '{unique_id}', '{summary}', '{detail}', {is_done}, null){',' if i < total_todos -1 else ''}"
     print(value_fmt)
