e927f33b0a75f87302df20ef5591fd6e6e91c171
Dean Agan <agandfr@gmail.com>
2021-11-29 19:54:51 +1100

    Include auth action creators to store

--- react-frontend/src/store/index.ts
+++ react-frontend/src/store/index.ts
@@ -1,4 +1,5 @@
 export * as todoActionCreators from "./action-creators/todoActionCreators";
 export * as usersActionCreators from "./action-creators/usersActionCreators";
+export * as authActionCreators from "./action-creators/authActionCreators";
 export * from "./store";
 export * from "./reducers/index";
