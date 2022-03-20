a19169f0e4196f1d884d772c11a355338f7c5d2a
Dean Agan <agandfr@gmail.com>
2022-03-20 17:20:21 +1100

    Removed weather forecast controller boilerplate

--- aspdotnet-cqrs-backend/TodoBackend.Api/Features/Todos/TodosController.cs
+++ aspdotnet-cqrs-backend/TodoBackend.Api/Features/Todos/TodosController.cs
@@ -7,32 +7,32 @@ using Microsoft.Extensions.Logging;
 namespace aspdotnet_cqrs_backend.Controllers
 {
   [ApiController]
-    [Route("[controller]")]
-    public class TodosController : ControllerBase
+  [Route("[controller]")]
+  public class TodosController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<TodosController> _logger;
 
-        public TodosController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+    public TodosController(ILogger<TodosController> logger)
+    {
+      _logger = logger;
     }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+      var rng = new Random();
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+  }
 }
